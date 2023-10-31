080d388b27a22b2de311ab4ca81d306f6afc9090
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-10-30 20:13:06 -0600

    Prueba Fallida

--- obra/views.py
+++ obra/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.models import User
+from .models import Profile
 from .forms import Usuario
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, logout
 from django.db import IntegrityError
 from django.contrib.auth.decorators import login_required  # *para proteger las rutas
 # Create your views here.
@@ -20,46 +20,46 @@ def about(request):
     return render(request, 'about.html')
 
 # ! registro de usuario
+# * modififcado
 
 
 def new_usuario(request):
     if request.method == 'GET':
         return render(request, 'registro_Usuario.html', {
-            'form': Usuario
+            'form': Usuario()
         })
-    else:
-        if request.POST['password1'] == request.POST['password2']:
-            try:  # * Aqui se registra el usuario
-                user = User.objects.create_user(
-                    username=request.POST['username'], password=request.POST['password1'])
-                user.save()
+    elif request.method == 'POST':
+        form = Usuario(request.POST)
+        if form.is_valid():
+            try:
+                user = form.save()  # Esto crea un nuevo usuario y un perfil asociado
                 login(request, user)
                 return redirect('home')
             except IntegrityError:
                 return render(request, 'registro_Usuario.html', {
-                    'form': Usuario,
-                    'error': 'Usuario no existe'
+                    'form': form,
+                    'error': 'Usuario ya existe'
                 })
-        return render(request, 'registro_Usuario.html')
+    return render(request, 'registro_Usuario.html', {'form': Usuario()})
 
 
 #! Autenticar e incio de sesion del usuario
+# * modificado
 def autenticar(request):
     if request.method == 'GET':
         return render(request, 'inicio_sesion.html', {
-            'form': AuthenticationForm
+            'form': AuthenticationForm()
         })
-    else:
-        user = authenticate(
-            request, username=request.POST['username'], password=request.POST['password'])
-        if user is None:  # Si el usuario no existe
-            return render(request, 'inicio_sesion.html', {
-                'form': AuthenticationForm,
-                'error': 'Usuario o contraseña incorrecta'
-            })
-        else:  # si SI existe lo reenvia a TASKS
+    elif request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
             return redirect('home')
+        return render(request, 'inicio_sesion.html', {
+            'form': form,
+            'error': 'Usuario o contraseña incorrecta'
+        })
 
 
 #! Cerrar sesion usuario
