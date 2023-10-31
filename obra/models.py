401c4c5ead8a92b2bb1cee25f2287b5019e29c7d
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-10-30 18:45:16 -0600

    Modelo Usuario

--- obra/models.py
+++ obra/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import User, Group
+from django.db.models.signals import post_save
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile', verbose_name='Usuario')
+    groups = models.ManyToManyField(
+        Group, blank=True, verbose_name='Grupos del Usuario')
+    correo = models.CharField(
+        max_length=150, null=True, blank=True, verbose_name='Correo Electronico')
+
+    class Meta:
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfiles'
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.user.username
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
