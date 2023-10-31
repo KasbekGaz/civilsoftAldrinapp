8cb6e5a82f2f233f543d810944eca5f8e3437ed6
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-10-30 19:21:38 -0600

    App.py concretado

--- obra/apps.py
+++ obra/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ObraConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'obra'
+    verbose_name = 'Perfiles'
+
+    def ready(self):
+        import obra.signals
